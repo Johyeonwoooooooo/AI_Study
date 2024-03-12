0ff3141cd6cd722e0e446f37159be53c6c215c8a
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-03-12 18:06:11 +0900

    GPT_API

--- Study/GPT_API.py
+++ Study/GPT_API.py
@@ -1,8 +1,18 @@
+# -*- coding: utf-8 -*-
 import openai
+import time
 
 openai.api_key = "${apikey}"
 
 messages = []
+predefined_messages = ['앞으로 실연당한 사람처럼 슬프게 대답해봐']
+
+for msg in predefined_messages:
+    messages.append({"role": "user", "content": msg})
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    assistant_content = completion.choices[0].message["content"].strip()
+    messages.append({"role": "assistant", "content": assistant_content})
+
 while True:
     user_content = input("user : ")
     messages.append({"role": "user", "content": f"{user_content}"})
@@ -14,4 +24,5 @@ while True:
     messages.append({"role": "assistant", "content": f"{assistant_content}"})
 
     print(f"GPT : {assistant_content}")
+    time.sleep(15)
 
